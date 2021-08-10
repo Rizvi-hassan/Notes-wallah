73886f00e28f19a50d7054e461e66c65d369be3a
Rizvi Hassan <mdrizvihassanansari@gmail.com>
2021-08-10 06:56:55 +0530

    added notes of computer

--- static/modules/class11_sub_ch.js
+++ static/modules/class11_sub_ch.js
@@ -147,6 +147,10 @@ function port_chemistry(app){
 }
 
 function port_computer(app){
+    app.get('/computer_programs', (req, res) => {
+        res.status(200).render('class_11/computer/prog.pug');
+    });
+
     app.get('/computer_ch1_11', (req, res) => {
         res.status(200).render('class_11/computer/ch1.pug');
     });
